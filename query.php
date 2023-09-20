cdebc04321d54adc4bd1b62da5a86e72c54a0a6d
mogaya <mogayainnocent@gmail.com>
2023-09-20 18:13:18 +0300

    STK Query

--- query.php
+++ query.php
@@ -9,7 +9,7 @@ $passkey = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919";
 // ENCRIPT  DATA TO GET PASSWORD
 $Password = base64_encode($BusinessShortCode . $passkey . $Timestamp);
 //THIS IS THE UNIQUE ID THAT WAS GENERATED WHEN STK REQUEST INITIATED SUCCESSFULLY
-$CheckoutRequestID = 'ws_CO_03072023054410314768168060';
+$CheckoutRequestID = 'ws_CO_20092023175748181704379546';
 $queryheader = ['Content-Type:application/json', 'Authorization:Bearer ' . $access_token];
 # initiating the transaction
 $curl = curl_init();
@@ -25,7 +25,7 @@ $data_string = json_encode($curl_post_data);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($curl, CURLOPT_POST, true);
 curl_setopt($curl, CURLOPT_POSTFIELDS, $data_string);
-echo $curl_response = curl_exec($curl);
+$curl_response = curl_exec($curl);
 $data_to = json_decode($curl_response);
 if (isset($data_to->ResultCode)) {
   $ResultCode = $data_to->ResultCode;
