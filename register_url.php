521cca0d3c08937ee487ee3ae80d245141049cdc
mogaya <mogayainnocent@gmail.com>
2023-09-20 18:58:57 +0300

    C2B

--- register_url.php
+++ register_url.php
@@ -2,9 +2,9 @@
 //INCLUDE ACCESS TOKEN FILE 
 include 'accessToken.php';
 $registerurl = 'https://sandbox.safaricom.co.ke/mpesa/c2b/v1/registerurl';
-$BusinessShortCode = '';
-$confirmationUrl = 'https://1c95-105-161-14-223.ngrok-free.app/Daraja-Api/confirmation_url.php';
-$validationUrl = 'https://1c95-105-161-14-223.ngrok-free.app/Daraja-Api/validation_url.php';
+$BusinessShortCode = '174379';
+$confirmationUrl = 'https://e893-41-89-16-2.ngrok-free.app/daraja/confirmation_url.php';
+$validationUrl = 'https://e893-41-89-16-2.ngrok-free.app/daraja/validation_url.php';
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, $registerurl);
 curl_setopt($curl, CURLOPT_HTTPHEADER, array(
